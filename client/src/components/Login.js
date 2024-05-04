4d0e8441ccb38560029e014c346ff056725ab6c0
alyssaabls <maria_alessandra_abalos@dlsu.edu.ph>
2024-05-04 15:41:47 +0800

    Final design of Login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,27 @@
 import React from 'react';
+import ImageStack from './imageStack';
 import '../styles/LoginPage.css';
-import logo from '../styles/logo-nobg.png'
+import logo from '../styles/logo-nobg.png';
 
 const LoginPage = () => {
   return (
-    <div className="login-container">
-      <header className="login-header">
-      <img src={logo} alt="AnimoHub Logo" className="login-logo" />
-        AnimoHub Digital Repository System
-      </header>
-      <div className="login-body">
-        <button className="google-login">Sign in using Google</button>
-        <div className="or-section">OR</div>
-        <form className="login-form">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">LOGIN</button>
-        </form>
-        <div className="forgot-password">Forgot Password?</div>
+    <div className="main-layout">
+      <ImageStack />
+      <div className="login-container">
+        <header className="login-header">
+          <img src={logo} alt="AnimoHub Logo" className="login-logo" />
+          AnimoHub Digital Repository System
+        </header>
+        <div className="login-body">
+          <button className="google-login">Sign in using Google</button>
+          <div className="or-section">OR</div>
+          <form className="login-form">
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">LOGIN</button>
+          </form>
+          <div className="forgot-password">Forgot Password?</div>
+        </div>
       </div>
     </div>
   );
---
+++ client/src/components/imageStack.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/stack.css';
+import image1 from '../styles/compsci.jpeg';
+import image2 from '../styles/altdsi.png';
+import image3 from '../styles/building.jpeg'
+
+
+const ImageStack = () => {
+  return (
+    <div className="image-stack">
+       <img src={image1} alt="AnimoHub Logo" className="compsci" />
+       <img src={image2} alt="AnimoHub Logo" className="altdsi" />
+       <img src={image3} alt="AnimoHub Logo" className="building" />
+      
+     
+    </div>
+  );
+};
+
+export default ImageStack;
