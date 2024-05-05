c2fe4cdf84150fab28fc999b6e38b7a12e3be887
alyssaabls <maria_alessandra_abalos@dlsu.edu.ph>
2024-05-05 11:50:41 +0800

    Added Register.js page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Description from './components/Description';
+import Register from './components/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/description" element={<Description />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/" element={<Login />} />
     </Routes>
   );
---
+++ client/src/components/Register.js
@@ -0,0 +1,14 @@
+// use this file for editing frontend @ Sean
+
+
+import React from 'react';
+
+const Register = () => {
+    return (
+      <div>
+       <h1>Register</h1>
+      </div>
+    );
+  };
+  
+export default Register;
