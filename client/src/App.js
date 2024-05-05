0d7d91bcd2c139d156c55172e356b85d9f673e23
alyssaabls <maria_alessandra_abalos@dlsu.edu.ph>
2024-05-05 11:31:39 +0800

    Initial page and linking to desc page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'; 
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Description from './components/Description';
 
 function App() {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/description" element={<Description />} />
       <Route path="/" element={<Login />} />
     </Routes>
   );
