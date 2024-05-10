c326e50ad9366e9825db03fe9176a8628051211f
alyssaabls <maria_alessandra_abalos@dlsu.edu.ph>
2024-05-10 19:23:28 +0800

    Initial design for Upload Form
    to be finalized after backend has been implemented

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import NavBar from './navbar';
+import '../styles/Upload.css'
 
 const Upload = () => {
     return (
         <div>
             <NavBar />
-           
+            <div className="upload-container">
+                <h1>Add a Document/Dataset</h1>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="submissionDate">Submission Date*</label>
+                        <input type="date" id="submissionDate" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="author">Author*</label>
+                        <input type="text" id="author" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="coAuthors">Co-Authors</label>
+                        <input type="text" id="coAuthors" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="title">Document/Dataset Title*</label>
+                        <input type="text" id="title" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="fileType">File Type*</label>
+                        <select id="fileType" required>
+                            <option value="">Select File Type</option>
+                            <option value="">DOCS</option>
+                            <option value="">IMG</option>
+                            <option value="">VID</option>
+                            <option value="">AUD</option>
+                            {/* Add more options as needed */}
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="fileUpload">Upload File*</label>
+                        <input type="file" id="fileUpload" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="accessType">Access Type*</label>
+                        <select id="accessType" required>
+                            <option value="">Select Access Type</option>
+                            <option value="">Public</option>
+                            <option value="">Private</option>
+                            <option value="">Internal-Access</option>
+                            <option value="">Confidential</option>
+                        </select>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
+
 export default Upload;
